34f41119b563cb5612b566f478e2b9f3a48c629a
chuangzl <chuangzl@192.168.31.151>
2017-04-27 11:50:21 +0800

    buf fix

--- other/download_images.sh
+++ other/download_images.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+aliyun_repo=registry.cn-qingdao.aliyuncs.com/chuangzl
+google_repo=gcr.io/google_containers
+	
 imageName=kube-apiserver
 imageVersion=v1.6.0
 docker pull $aliyun_repo/$imageName:$imageVersion    
 docker tag $aliyun_repo/$imageName:$imageVersion  $google_repo/$imageName-amd64:$imageVersion
 docker rmi $aliyun_repo/$imageName:$imageVersion  
-
