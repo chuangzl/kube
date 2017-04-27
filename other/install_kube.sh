f7bf573ab6f47cd53aa4420694184eeb493a9ee0
chuangzl <chuangzl@192.168.31.151>
2017-04-27 20:25:30 +0800

    bug fix

--- other/install_kube.sh
+++ other/install_kube.sh
@@ -2,6 +2,8 @@
 
 kubeadm init --service-cidr=172.16.128.0/24 --kubernetes-version=v1.6.0 --apiserver-advertise-address=10.63.55.7 --pod-network-cidr=10.244.0.0/16
 
+export KUBECONFIG=/etc/kubernetes/admin.conf 
+ 
 kubectl create -f ./kube-flannel-rbac.yml
 
 kubectl apply -f ./kube-flannel.yml
