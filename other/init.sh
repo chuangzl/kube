56a143d621e1d99ec2279929f4449cfb0bd44e2c
chuangzl <chuangzl@192.168.31.151>
2017-04-27 11:45:44 +0800

    bug fix

--- other/init.sh
+++ other/init.sh
@@ -27,7 +27,7 @@ google_repo=gcr.io/google_containers
 imageName=kube-apiserver
 imageVersion=v1.6.0
 docker pull $aliyun_repo/$imageName:$imageVersion    
-docker tag $aliyun_repo/$imageNamer:$imageVersion  $google_repo/$imageName-amd64:$imageVersion
+docker tag $aliyun_repo/$imageName:$imageVersion  $google_repo/$imageName-amd64:$imageVersion
 docker rmi $aliyun_repo/$imageName:$imageVersion  
 
 
